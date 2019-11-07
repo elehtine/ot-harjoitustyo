65477c345faa54890651acbca0d874916f326984
Elias Lehtinen <elias.lehtinen01@gmail.com>
2019-11-08 01:38:14 +0200

    Update gitignore

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,45 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+
+    @Test
+    public void alussaSaldoOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+
+    @Test
+    public void saldoKasvaa() {
+		kortti.lataaRahaa(250);
+        assertEquals("saldo: 12.50", kortti.toString());
+    }
+
+    @Test
+    public void saldonVaheneminenPalauttaaTrue() {
+		assertTrue(kortti.otaRahaa(250));
+    }
+
+    @Test
+    public void eiVoiOttaaRahaaPalauttaaFalse() {
+		assertFalse(kortti.otaRahaa(1500));
+    }
+
+    @Test
+    public void saldoVahenee() {
+		kortti.otaRahaa(250);
+        assertEquals("saldo: 7.50", kortti.toString());
+    }
+
+    @Test
+    public void eiNegatiivinenSaldo() {
+		kortti.otaRahaa(1500);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
 }
